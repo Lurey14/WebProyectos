870c102d4eb8ccd5aee39d55d0e13500e006458d
Lurey14 <noemy.nmg@gmail.com>
2025-09-16 20:07:50 -0400

    HomeController-Index_1

--- AplicacionPedidos/Program.cs
+++ AplicacionPedidos/Program.cs
@@ -1,7 +1,13 @@
+using AplicacionPedidos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DBPedidosContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
