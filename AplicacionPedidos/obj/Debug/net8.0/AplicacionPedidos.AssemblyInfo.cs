659c3659a0cb926ed19d050bcba7922f89c6a833
Lurey14 <noemy.nmg@gmail.com>
2025-09-18 16:13:20 -0400

    mejoras_users_diseño

--- AplicacionPedidos/obj/Debug/net8.0/AplicacionPedidos.AssemblyInfo.cs
+++ AplicacionPedidos/obj/Debug/net8.0/AplicacionPedidos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionPedidos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb823a18695be75d73df028066ede96f8faf7c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6d505c70bfccda92dd263c005c1224cbab845c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionPedidos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionPedidos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
